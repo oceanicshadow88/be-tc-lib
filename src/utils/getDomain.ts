5cfba0b9d70e5b576a0374bd1b142c976753e67f
ChloeD-L <linduan1018@gmail.com>
2025-05-15 20:23:44 +1000

    feat: add request domain resolver & tenant from DB using host utility

---
+++ src/utils/getDomain.ts
@@ -0,0 +1,24 @@
+import { Request } from "express";
+import { getDomainConfig } from "../middleware/domainConfig";
+
+export const getDomain = (req: Request, isLocalEnv: boolean): string => {
+  const domainConfig = getDomainConfig();
+  const hasConnectedTenant = domainConfig.connectTenantOrigin && domainConfig.connectTenantOrigin !== "";
+
+  if (isLocalEnv) {
+    if (hasConnectedTenant) {
+      return `${domainConfig.protocol}${domainConfig.connectTenantOrigin}.${domainConfig.mainDomain}`;
+    }
+    if (req.headers.origin) {
+      return req.headers.origin;
+    }
+    if (domainConfig.postman_testing!.toLocaleLowerCase() === "true") {
+      return "localhost";
+    }
+    return "";
+  }
+
+  return hasConnectedTenant
+    ? `${domainConfig.protocol}${domainConfig.connectTenantOrigin}.${domainConfig.mainDomain}`
+    : (req.headers.origin as string);
+};
